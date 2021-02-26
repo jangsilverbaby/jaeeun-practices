18fd504241eb74e30bee16b9d05b6db1d71da869
eunaejang <dmsdo1234@gmail.com>
2021-02-26 23:04:23 +0900

    받은 알림 처리하기(UserNotification 프레임워크)

--- Msg-Notification/Msg-Notification/AppDelegate.swift
+++ Msg-Notification/Msg-Notification/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             //경고창, 배지, 사운드를 사용하는 알림 환경 정보를 생성하고, 사용자 동의 여부 창을 실행
             let notiCenter = UNUserNotificationCenter.current()
             notiCenter.requestAuthorization(options: [.alert, .badge, .sound]){(didAllow, e) in }
+            //아래 코드의 의미는 "알림 센터와 관련하여 뭔가 사건이 발생하면 나(앱 델리게이트)한테 알려줘!" 이다.
+            notiCenter.delegate = self
         } else {
             //경고창, 배지, 사운드를 사용하는 알림 환경 정보를 생성하고, 이를 애플리케이션에 저장
             let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
@@ -37,6 +39,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    //앱 실행 도중에 알림 메세지가 도착한 경우
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if notification.request.identifier == "wakeup" {
+            let userInfo = notification.request.content.userInfo
+            print(userInfo["name"]!)
+        }
+        //알림 배너 띄어주기
+        completionHandler([.banner, .badge, .sound])
+    }
+    
+    //사용자가 알림 메세지를 클릭했을 경우
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.identifier == "wakeup" {
+            let userInfo = response.notification.request.content.userInfo
+            print(userInfo["name"]!)
+        }
+        completionHandler()
     }
 }
 
