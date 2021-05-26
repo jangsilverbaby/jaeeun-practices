b92692c0495e38b258599f5976ce5b34ee4ad379
eunaejang <dmsdo1234@gmail.com>
2021-05-26 12:33:13 +0900

    Extension을 이용한 델리게이트 패턴 코딩

--- Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet var imgView: UIImageView!
     
@@ -23,7 +23,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // 이미지 피커 컨트롤러 실행
         self.present(picker, animated: false)
     }
-    
+}
+//MARK:- 이미지 피커 컨트롤러 델리게이트 메소드
+extension ViewController: UIImagePickerControllerDelegate {
     // 이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // 이미지 피커 컨트롤러 창 닫기 <- 가장 먼저 해주어야할 일
@@ -34,7 +36,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.present(alert, animated: false)
         })
         // 피커 컨트롤러와 알림창은 모두 ViewController에서 실행하는 컨트롤러이기 떄문에 화면에 표시되기 위해 서로 경합을 할 수밖에 없는데, 만약 이미지 피터 컨트롤러가 조금 늦게 닫히고 그동안 알림창 구문이 호출될 경우 알림창이 실행되지 않는 버스가 생길 수 있기 때문에 dismiss 메소드를 사용하여 이미지 피커 컨트롤러 창이 완전히 닫힌 후에 다음 로직을 실행할 수 있도록 수정
-        
     }
     // 이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -46,4 +47,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
 }
-
+//MARK:- 내비게이션 컨트롤러 델리게이트 메소드
+extension ViewController: UINavigationControllerDelegate {
+}
