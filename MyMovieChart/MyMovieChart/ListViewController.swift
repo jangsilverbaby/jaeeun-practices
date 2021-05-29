5d38afb83d6703f22a5df2e8f8cb5bb4ea4024eb
eunaejang <dmsdo1234@gmail.com>
2021-05-29 09:22:15 +0900

    프로토타입 셀을 이용한 테이블 뷰 실습

---
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ListViewController.swift
+//  MyMovieChart
+//
+//  Created by eunae on 2021/05/29.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+}
---
+++ MyMovieChart/MyMovieChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by eunae on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
