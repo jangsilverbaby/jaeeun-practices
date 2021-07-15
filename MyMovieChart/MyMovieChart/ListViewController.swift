f424c948dab47c3800748b7dda6613ec65b5db06
eunaejang <dmsdo1234@gmail.com>
2021-07-15 13:47:05 +0900

    데이터 소스 만들기

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -8,5 +8,35 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-
+    // 테이블 뷰를 구성할 리스트 데이터
+    var list = [MovieVo]()
+    
+    override func viewDidLoad() {
+        // 첫번째 행
+        var mvo = MovieVo()
+        mvo.title = "다크나이드"
+        mvo.desctiption = "영웅물에 철학에 음악까지 더해져 예술이 되다."
+        mvo.opendate = "2008-09-04"
+        mvo.rating = 8.95
+        // 배열에 추가
+        self.list.append(mvo)
+        
+        // 두번째 행
+        mvo = MovieVo()
+        mvo.title = "호우시절"
+        mvo.desctiption = "떄룰 알고 내리는 좋은 비"
+        mvo.opendate = "2009-10-08"
+        mvo.rating = 7.31
+        // 배열에 추가
+        self.list.append(mvo)
+        
+        // 세번째 행
+        mvo = MovieVo()
+        mvo.title = "말할 수 없는 비밀"
+        mvo.desctiption = "여기서 너까지 다섯 걸음"
+        mvo.opendate = "2015-05-07"
+        mvo.rating = 9.19
+        // 배열에 추가
+        self.list.append(mvo)
+    }
 }
---
+++ MyMovieChart/MyMovieChart/MovieVo.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVo.swift
+//  MyMovieChart
+//
+//  Created by eunae on 2021/07/14.
+//
+
+import Foundation
+
+class MovieVo {
+    var thumbnail: String? // 영화 섬네일 이미지 주소
+    var title: String? // 영화 제목
+    var desctiption: String? // 영화 설명
+    var detail: String? // 상세정보
+    var opendate: String? // 개봉일
+    var rating: Double? // 평점
+}
