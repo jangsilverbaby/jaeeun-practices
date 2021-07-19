ef7bb1275b79e95408ea99df8864ae93eb72dc68
eunaejang <dmsdo1234@gmail.com>
2021-07-19 13:08:52 +0900

    테이블 뷰와 데이터 소스 연동

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -8,35 +8,44 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-    // 테이블 뷰를 구성할 리스트 데이터
-    var list = [MovieVo]()
+    // 튜플 아이템으로 구성된 데이터 세트
+    var dataset = [
+        ("다크나이드","영웅물에 철학에 음악까지 더해져 예술이 되다.","2008-09-04",8.95),
+        ("호우시절","때를 알고 내리는 좋은 비","2009-10-08",7.31),
+        ("말할 수 없는 비밀","여기서 너까지 다섯 걸음","2015-05-07",9.19)
+    ]
     
-    override func viewDidLoad() {
-        // 첫번째 행
-        var mvo = MovieVo()
-        mvo.title = "다크나이드"
-        mvo.desctiption = "영웅물에 철학에 음악까지 더해져 예술이 되다."
-        mvo.opendate = "2008-09-04"
-        mvo.rating = 8.95
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        // 두번째 행
-        mvo = MovieVo()
-        mvo.title = "호우시절"
-        mvo.desctiption = "떄룰 알고 내리는 좋은 비"
-        mvo.opendate = "2009-10-08"
-        mvo.rating = 7.31
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        // 세번째 행
-        mvo = MovieVo()
-        mvo.title = "말할 수 없는 비밀"
-        mvo.desctiption = "여기서 너까지 다섯 걸음"
-        mvo.opendate = "2015-05-07"
-        mvo.rating = 9.19
-        // 배열에 추가
-        self.list.append(mvo)
+    // 테니블 뷰를 구성할 리스트 데이터
+    lazy var list: [MovieVo] = {
+        var datalist = [MovieVo]()
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVo()
+            mvo.title = title
+            mvo.desctiption = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+
+            datalist.append(mvo)
+        }
+        return datalist
+    }()
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = list[indexPath.row]
+        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        //서브타이틀에 데이터 연결
+        cell.detailTextLabel?.text = row.desctiption
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row)번째 행입니다.")
     }
 }
