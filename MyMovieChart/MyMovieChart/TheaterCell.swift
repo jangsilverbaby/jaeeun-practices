e0977d36f76d723a6fd89e10c66ec1927b895cc0
eunaejang <dmsdo1234@gmail.com>
2021-08-05 15:26:31 +0900

    영화관 목록 화면 구현하기

---
+++ MyMovieChart/MyMovieChart/TheaterCell.swift
@@ -0,0 +1,16 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by eunae on 2021/08/05.
+//
+
+import UIKit
+class TheaterCell: UITableViewCell {
+    // 상영관명
+    @IBOutlet var name: UILabel!
+    // 연락처
+    @IBOutlet var addr: UILabel!
+    // 주소
+    @IBOutlet var tel: UILabel!
+}
