78baf59b51fa40c0a4105ca2b084697297cf51a2
eunaejang <dmsdo1234@gmail.com>
2021-08-18 22:19:39 +0900

    커스텀 코드를 사용하여 뷰 컨트롤러에 버튼 추가하기

--- Button/Button/ViewController.swift
+++ Button/Button/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // 버튼 객체를 생성하고, 속성을 설정한다.
+        // 1. 버튼 타입을 인자값으로 하여 버튼 인스턴스를 생성한다.
+        let btn = UIButton(type: .system)
+        // 2. 버튼 영역을 설정하여 frame 속성에 대입한다.
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
+        // 3. setTitl(_:for:) 메소드를 이용하여 "테스트 버튼"이라는 문구를 타이틀로 설정한다.
+        btn.setTitle("테스트 버튼", for: .normal)
+        
+        // 버튼을 수평 중앙 정렬한다.
+        btn.center = CGPoint(x: view.frame.size.width / 2, y: 100)
+        
+        // 루트 뷰에 버튼을 추가한다.
+        view.addSubview(btn)
     }
-
-
+    
 }
 
