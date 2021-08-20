9e8eace4cfae6aac7b69b49cc97caa280466d12d
eunaejang <dmsdo1234@gmail.com>
2021-08-20 20:14:56 +0900

    탭 바 커스터마이징을 위한 준비 과정

--- TabBar/TabBar/ThirdViewController.swift
+++ TabBar/TabBar/ThirdViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ThirdViewController.swift
 //  TabBar
 //
 //  Created by eunae on 2021/08/20.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ThirdViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
         
         // 2. title 레이블 속성 설정
-        title.text = "첫번째 탭"
+        title.text = "세번째 탭"
         title.textColor = .red // 텍스트는 빨간색으로
         title.textAlignment = .center // 레이블 내에서 중앙 정렬로
         title.font = UIFont.boldSystemFont(ofSize: 14) // 폰트는 System Font, 14pt
@@ -29,9 +29,7 @@ class ViewController: UIViewController {
         
         // 5. 수퍼 뷰에 추가
         view.addSubview(title)
-            
     }
 
 
 }
-
