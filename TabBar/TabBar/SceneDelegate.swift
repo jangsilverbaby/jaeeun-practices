7c4343e37435d06a9033892c785df48ba69bb690
eunaejang <dmsdo1234@gmail.com>
2021-08-23 08:51:46 +0900

    탭 바의 이미지와 색상 커스터마이징하기

--- TabBar/TabBar/SceneDelegate.swift
+++ TabBar/TabBar/SceneDelegate.swift
@@ -17,6 +17,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // 1. 루트 뷰 컨트롤러를 UITabBarController롤 캐스팅한다.
+        if let tbC = window?.rootViewController as? UITabBarController {
+            // 2. 탭 바에서 탭 바 아이템 배열을 가져온다.
+            if let tbItems = tbC.tabBar.items {
+                // 3. 탭 바 아이템에 커스텀 이미지를 등록한다.
+                tbItems[0].image = UIImage(named: "calendar")
+                tbItems[1].image = UIImage(named: "file-tree")
+                tbItems[2].image = UIImage(named: "photo")
+                
+                // 4. 탭 바 아이템에 타이틀을 설정한다.
+                tbItems[0].title = "calendar"
+                tbItems[1].title = "file-tree"
+                tbItems[2].title = "photo"
+            }
+            
+            // 5. 탭 바 아이템의 이미지 색상을 변경한다.
+            tbC.tabBar.tintColor = .white // 선택된 탭 바 아이템의 색상
+            tbC.tabBar.unselectedItemTintColor = .gray // 선택되지 않은 나머지 탭 바 아이템의 색상
+            
+            // 6. 탭 바에 배경 이미지를 설정한다.
+            tbC.tabBar.backgroundImage = UIImage(named: "menubar-bg-mini")?.stretchableImage(withLeftCapWidth: 5, topCapHeight: 16)
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
