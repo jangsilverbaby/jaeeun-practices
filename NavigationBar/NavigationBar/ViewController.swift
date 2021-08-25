39b2bd507b224566c68ec90faa358174d3b54446
eunaejang <dmsdo1234@gmail.com>
2021-08-25 12:10:01 +0900

    initial commit

---
+++ NavigationBar/NavigationBar/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NavigationBar
+//
+//  Created by eunae on 2021/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.initTitle()
+    }
+
+    func initTitle() {
+        
+    }
+
+}
+
