38045516b81aca54492751a27c0d6897a1ed93bd
eunaejang <dmsdo1234@gmail.com>
2021-08-26 10:50:27 +0900

    이미지가 포함된 알림창

--- Alert/Alert/MapAlertViewController.swift
+++ Alert/Alert/MapAlertViewController.swift
@@ -22,6 +22,17 @@ class MapAlertViewController: UIViewController {
         alertBtn.addTarget(self, action: #selector(mapAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(alertBtn)
+        
+        // 이미지 알림창 버튼 생성
+        let imageBtn = UIButton(type: .system)
+        
+         //이미지 알림창 버튼 속성 설정
+        imageBtn.frame = CGRect(x: 0, y: 200, width: 100, height: 30)
+        imageBtn.center.x = self.view.frame.width / 2
+        imageBtn.setTitle("Image Alert", for: .normal)
+        imageBtn.addTarget(self, action: #selector(imageAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(imageBtn)
     }
     
     @objc func mapAlert(_ sender: UIButton) {
@@ -41,6 +52,22 @@ class MapAlertViewController: UIViewController {
         alert.setValue(contentVC, forKey: "contentViewController")
         
         self.present(alert, animated: false)
+    }
+    
+    @objc func imageAlert(_ sender: Any) {
+        // 경고창 객체를 생성하고, OK 및 Cancle 버튼을 추가한다.
+        let alert = UIAlertController(title: nil, message: "이번 글의 평점은 다음과 같습니다.", preferredStyle: .alert)
         
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        
+        // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러를 생성하고, 알림창에 등록한다.
+        let contentVC = ImageViewController()
+        alert.setValue(contentVC, forKey: "contentViewController")
+        
+        self.present(alert, animated: false)
     }
 }
