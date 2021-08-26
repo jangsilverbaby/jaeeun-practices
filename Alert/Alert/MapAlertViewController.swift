b41ddd5aebe92c46568460fe125615fa9fe3c145
eunaejang <dmsdo1234@gmail.com>
2021-08-26 13:05:48 +0900

    컨트롤이 사용된 알림참

--- Alert/Alert/MapAlertViewController.swift
+++ Alert/Alert/MapAlertViewController.swift
@@ -26,13 +26,25 @@ class MapAlertViewController: UIViewController {
         // 이미지 알림창 버튼 생성
         let imageBtn = UIButton(type: .system)
         
-         //이미지 알림창 버튼 속성 설정
+        // 이미지 알림창 버튼 속성 설정
         imageBtn.frame = CGRect(x: 0, y: 200, width: 100, height: 30)
         imageBtn.center.x = self.view.frame.width / 2
         imageBtn.setTitle("Image Alert", for: .normal)
         imageBtn.addTarget(self, action: #selector(imageAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(imageBtn)
+        
+        // 슬라이더 알림창 버튼 생성
+        let sliderBtn = UIButton(type: .system)
+        
+        // 이미지 알림창 버튼 속성 설정
+        sliderBtn.frame = CGRect(x: 0, y: 250, width: 100, height: 30)
+        sliderBtn.center.x = self.view.frame.width / 2
+        sliderBtn.setTitle("Slider Alert", for: .normal)
+        sliderBtn.addTarget(self, action: #selector(sliderAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(sliderBtn)
+        
     }
     
     @objc func mapAlert(_ sender: UIButton) {
@@ -67,6 +79,25 @@ class MapAlertViewController: UIViewController {
         // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러를 생성하고, 알림창에 등록한다.
         let contentVC = ImageViewController()
         alert.setValue(contentVC, forKey: "contentViewController")
+        
+        self.present(alert, animated: false)
+    }
+    
+    @objc func sliderAlert(_ sender: Any) {
+        // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러를 생성
+        let contentVC = ControlViewController()
+        
+        // 경고창 객체를 생성
+        let alert = UIAlertController(title: nil, message: "이번 글의 평점을 입력해주세요.", preferredStyle: .alert)
+        
+        // 컨트롤 뷰 컨트롤러를 알림창에 등록한다.
+        alert.setValue(contentVC, forKey: "contentViewController")
+        
+        // OK 버튼을 추가한다.
+        let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+            print(">>> sliderValue = \(contentVC.sliderValue)")
+        }
+        alert.addAction(okAction)
         
         self.present(alert, animated: false)
     }
