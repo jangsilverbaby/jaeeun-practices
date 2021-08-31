75e63b924ef8946aa900c54349f3c151817f3c0b
eunaejang <dmsdo1234@gmail.com>
2021-08-31 11:30:46 +0900

    커스터마이징을 위한 속성 추가하기

--- CSStepper/CSStepper/CSStepper.swift
+++ CSStepper/CSStepper/CSStepper.swift
@@ -13,10 +13,39 @@ public class CSStepper: UIView {
     public var leftBtn = UIButton(type: .system) // 좌측 버튼
     public var rightBtn = UIButton(type: .system) // 우측 버튼
     public var centerLabel = UILabel() // 중앙 레이블
+    
+    // 증감값 단위
+    public var stepValue: Int = 1
+    
+    // 최대값과 최소값
+    public var maximumValue: Int = 100
+    public var minimumValue: Int = -100
+    
     // 스테퍼의 현재값을 저장할 변수
     public var value: Int = 0 {
         didSet { // 프로퍼티 옵저버 : 프로퍼티의 값이 바뀌면 자동으로 호출된다.
             self.centerLabel.text = String(value)
+        }
+    }
+    
+    // 좌측 버튼의 타이틀 속성
+    public var leftTitle: String = "↓" {
+        didSet {
+            self.leftBtn.setTitle(leftTitle, for:.normal)
+        }
+    }
+    
+    // 우측 버튼의 타이틀 속성
+    public var rightTitle: String = "↑" {
+        didSet {
+            self.rightBtn.setTitle(rightTitle, for: .normal)
+        }
+    }
+    
+    // 센터 영역의 색상
+    public var bgColor: UIColor = UIColor.cyan {
+        didSet {
+            self.centerLabel.backgroundColor = backgroundColor
         }
     }
     
@@ -40,14 +69,14 @@ public class CSStepper: UIView {
         
         // 좌측 다운 버튼 속성 설정
         self.leftBtn.tag = -1 // 태그값에 -1을 부여
-        self.leftBtn.setTitle("↓", for: .normal) // 버튼의 타이틀
+        self.leftBtn.setTitle(self.leftTitle, for: .normal) // 버튼의 타이틀
         self.leftBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20) // 버튼 타이틀의 폰트
         self.leftBtn.layer.borderWidth = borderWidth // 버튼 테두리 두께
         self.leftBtn.layer.borderColor = borderCololr // 버튼 테두리 색상 - 파란색
         
         // 우측 다운 버튼 속성 설정
         self.rightBtn.tag = 1 // 태그값에 1을 부여
-        self.rightBtn.setTitle("↑", for: .normal) // 버튼의 타이틀
+        self.rightBtn.setTitle(self.rightTitle, for: .normal) // 버튼의 타이틀
         self.rightBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20) // 버튼 타이틀의 폰트
         self.rightBtn.layer.borderWidth = borderWidth // 버튼 테두리 두께
         self.rightBtn.layer.borderColor = borderCololr // 버튼 테두리 색상 - 파란색
@@ -56,7 +85,7 @@ public class CSStepper: UIView {
         self.centerLabel.text = String(value) // 컨트롤의 현재값을 문자열로 변환하여 표시
         self.centerLabel.font = UIFont.systemFont(ofSize: 16) // 레이블의 폰트
         self.centerLabel.textAlignment = .center // 가운데 정렬
-        self.centerLabel.backgroundColor = .cyan // 배경 색상 지정
+        self.centerLabel.backgroundColor = self.bgColor // 배경 색상 지정
         self.centerLabel.layer.borderWidth = borderWidth // 버튼 테두리 두께
         self.centerLabel.layer.borderColor = borderCololr // 버튼 테두리 색상 - 파란색
         
@@ -89,7 +118,20 @@ public class CSStepper: UIView {
     
     // value 속성의 값을 변경하는 메소드
     @objc public func valueChange(_ sender: UIButton) {
+        // 스테퍼의 값을 변경하기 전에, 미리 최소값과 최대값 범위를 벗어나지 않는지 체크한다.
+        let sum = self.value + (sender.tag * self.stepValue)
+        
+        // 더한 결과가 최대값보다 크면 값을 변경하지 않고 종료
+        if sum > self.maximumValue {
+            return
+        }
+        
+        // 더한 결과가 최소값보다 작으면 값을 변경하지 않고 종료
+        if sum < self.minimumValue {
+            return
+        }
+        
         // 현재의 value 값에 +1 또는 -1 한다.
-        self.value += sender.tag
+        self.value += (sender.tag * self.stepValue)
     }
 }
