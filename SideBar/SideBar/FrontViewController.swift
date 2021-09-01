f19d0f10f373c1c23c5ec2702c895c1cd908f5e0
eunaejang <dmsdo1234@gmail.com>
2021-09-01 20:55:58 +0900

    SWRevealViewController 라이브러리 실습 준비

---
+++ SideBar/SideBar/FrontViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FrontViewController.swift
+//  SideBar
+//
+//  Created by eunae on 2021/09/01.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
