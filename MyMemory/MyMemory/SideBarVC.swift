88e260ea33aacdb91431280755fe784f215c557e
eunaejang <dmsdo1234@gmail.com>
2021-09-07 12:37:27 +0900

    입력 화면 전환하기

--- MyMemory/MyMemory/SideBarVC.swift
+++ MyMemory/MyMemory/SideBarVC.swift
@@ -78,4 +78,15 @@ class SideBarVC: UITableViewController {
         cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 { // 선택된 행이 새글 작성 메뉴일 때
+            let uv = self.storyboard?.instantiateViewController(identifier: "MemoForm")
+            let target = self.revealViewController().frontViewController as! UINavigationController
+            target.pushViewController(uv!, animated: true)
+            
+            // 사이드 바를 닫아준다.
+            self.revealViewController().revealToggle(self)
+        }
+    }
 }
