22ba5a9fabccdafe0d7fd64011255a367d9412a1
eunaejang <dmsdo1234@gmail.com>
2021-09-08 23:01:03 +0900

    Initial Commit

---
+++ Plist/Plist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plist
+//
+//  Created by eunae on 2021/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
