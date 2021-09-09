acabc31f08a47d72b2069bb40b73a0648f64cb2b
eunaejang <dmsdo1234@gmail.com>
2021-09-09 17:02:08 +0900

    데이터 저장 및 불러오기

--- UserDefaults/UserDefaults/ListViewController.swift
+++ UserDefaults/UserDefaults/ListViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class ListViewController: UITableViewController {
     @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var gender: UILabel!
-    @IBOutlet weak var married: UIView!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var married: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        let plist = UserDefaults.standard
+        
+        // 저장된 값을 꺼내어 각 컨트롤러에 설정한다.
+        self.name.text = plist.string(forKey: "name") // 이름
+        self.gender.selectedSegmentIndex = plist.integer(forKey: "gender") // 성별
+        self.married.isOn = plist.bool(forKey: "married") // 결혼 여부
+    }
     
     @IBAction func changeGender(_ sender: UISegmentedControl) {
+        let value = sender.selectedSegmentIndex // 0이면 남자, 1이면 여자
+        
+        let plist = UserDefaults.standard // 기본 저장소 객체를 가져온다.
+        plist.set(value, forKey: "gender") // "gender"라는 키로 값을 저장한다.
+        plist.synchronize() // 동기화 처리
     }
     @IBAction func changeMarried(_ sender: UISwitch) {
+        let value = sender.isOn // true면 기혼, false면 미혼
+        
+        let plist = UserDefaults.standard // 기본 저장소 객체를 가져온다.
+        plist.set(value, forKey: "married") // "married"라는 키로 값을 저장한다.
+        plist.synchronize() // 동기화 처리
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -28,6 +48,14 @@ class ListViewController: UITableViewController {
             // 버튼 및 액션 추가
             alert.addAction(UIAlertAction(title: "OK", style: .default) { (_) in
                 // 사용자가 OK 버튼을 누르면 입력 필드에 입력된 값을 저장한다.
+                let value = alert.textFields?[0].text
+                
+                let plist = UserDefaults.standard // 기본 저장소 객체를 가져온다.
+                plist.setValue(value, forKey: "name") // "name"이라는 키로 값을 저장한다.
+                plist.synchronize() // 동기화 처리
+                
+                // 수정된 값을 이름 레이블에도 적용한다.
+                self.name.text = value
             })
             // 알림창을 띄운다.
             self.present(alert, animated: false, completion: nil)
