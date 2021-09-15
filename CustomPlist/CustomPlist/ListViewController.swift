21668375616d2b6cfbaf75cd8268d4f932a3571e
eunaejang <dmsdo1234@gmail.com>
2021-09-15 13:16:04 +0900

    피커뷰

---
+++ CustomPlist/CustomPlist/ListViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ListViewController.swift
+//  CustomPlist
+//
+//  Created by eunae on 2021/09/15.
+//
+
+import UIKit
+
+class ListViewController : UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var account: UITextField!
+    
+    var accountlist = ["sqlpro@naver.com", "webmaster@rubypaper.co.kr", "abc1@gmail.com", "abc2@gmail.com", "abc3@gmail.com" ]
+    
+    override func viewDidLoad() {
+        let picker = UIPickerView()
+        
+        // 1. 피커 뷰의 델리게이트 객체 지정
+        picker.delegate = self
+        // 2. account 텍스트 필드 입력 방식을 가상 키보드 대신 피커 뷰로 설정
+        self.account.inputView = picker
+        
+        // 툴 바 객체 정의
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRect(x: 0, y: 0, width: 0, height: 35)
+        toolbar.barTintColor = .lightGray
+        
+        // 액세서리 뷰 영역에 툴바를 표시
+        self.account.inputAccessoryView = toolbar
+        
+        // 툴 바에 들어갈 닫기 버튼
+        let done = UIBarButtonItem()
+        done.title = "Done"
+        done.target = self
+        done.action = #selector(pickerDone)
+        
+        // 버튼을 툴 바에 추가
+        toolbar.setItems([done], animated: true)
+    }
+    
+    // 생성할 컴포넌트의 개수를 정의합니다.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // 지정된 컴포넌트가 가질 목록의 길이를 정의합니다.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.accountlist.count
+    }
+    
+    // 지정된 컴포넌트의 목록 각 행에 출력될 내용을 정의합니다.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.accountlist[row]
+    }
+    
+    // 지정된 컴포넌트의 목록 각 행을 사용자가 선택했을 때 실행할 액션을 정의합니다.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // 1. 선택된 계정값을 텍스트 필드에 입력
+        let account = self.accountlist[row] // 선택된 계정
+        self.account.text = account
+    }
+    
+    @objc func pickerDone(_ sender: Any) {
+        // 2. 입력 뷰를 닫음
+        self.view.endEditing(true)
+    }
+}
