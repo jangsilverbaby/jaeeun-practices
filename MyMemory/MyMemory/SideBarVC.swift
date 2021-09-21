e7265ebd4659fa9ae15bc1c4868d846a4f7f43fb
eunaejang <dmsdo1234@gmail.com>
2021-09-21 12:55:22 +0900

    사이드 바의 프로필 영역에 저장 데이터 적용하기

--- MyMemory/MyMemory/SideBarVC.swift
+++ MyMemory/MyMemory/SideBarVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class SideBarVC: UITableViewController {
+    let uinfo = UserInfoManager() // 개인 정보 관리 매니저
+    
     // 목록 데이터 배열
     let titles  = ["새글 작성하기", "친구 새글", "달력으로 보기", "공디사항", "통계", "계정 관리"]
     
@@ -25,6 +27,7 @@ class SideBarVC: UITableViewController {
     let emailLabel = UILabel() // 이메일 레이블
     let profileImage = UIImageView() // 프로필 이미지
     
+    // 화면이 '로딩'될 때에만 호출
     override func viewDidLoad() {
         // 테이블 뷰의 헤더 역할을 할 뷰를 정의한다.
         let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
@@ -35,7 +38,6 @@ class SideBarVC: UITableViewController {
         
         // 이름 레이블의 속성을 정의하고, 헤더 뷰에 추가한다.
         self.nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30) // 위치와 크기를 정의
-        self.nameLabel.text = "꼼꼼한 재은 씨" // 기본 텍스트
         self.nameLabel.textColor = .white // 텍스트 색상
         self.nameLabel.font = UIFont.boldSystemFont(ofSize: 15) // 폰트 사이즈
         self.nameLabel.backgroundColor = .clear // 배경 색상
@@ -43,7 +45,6 @@ class SideBarVC: UITableViewController {
         headerView.addSubview(self.nameLabel) // 헤더 뷰에 추가
         
         self.emailLabel.frame = CGRect(x: 70, y: 30, width: 100, height: 30) // 위치와 크기를 정의
-        self.emailLabel.text = "sqlpro@naver.com" // 기본 텍스트
         self.emailLabel.textColor = .white // 텍스트 색상
         self.emailLabel.font = UIFont.systemFont(ofSize: 11) // 폰트 사이즈
         self.emailLabel.backgroundColor = .clear // 배경 색상
@@ -51,14 +52,19 @@ class SideBarVC: UITableViewController {
         headerView.addSubview(self.emailLabel) // 헤더 뷰에 추가
         
         // 기존 이미지를 구현한다.
-        let defaultProfile = UIImage(named: "account.jpg")
-        self.profileImage.image = defaultProfile // 이미지 등록
         self.profileImage.frame = CGRect(x: 10, y: 10, width: 50, height: 50) // 위치와 크기를 정의
         self.profileImage.layer.cornerRadius = (self.profileImage.frame.width / 2) // 반원 형태로 라운딩
         self.profileImage.layer.borderWidth = 0 // 테두리 두께 0으로
         self.profileImage.layer.masksToBounds = true // 마스크 효과 : 기존의 이미지 위에 덧씌워서 일부를 가리는 역할을 하는 레이어를 의미
         
         headerView.addSubview(self.profileImage) // 헤더 뷰에 추가
+    }
+    
+    // 화면이 '표시'될 떄마다 호출
+    override func viewWillAppear(_ animated: Bool) {
+        self.nameLabel.text = self.uinfo.name ?? "Guest"
+        self.emailLabel.text = self.uinfo.account ?? ""
+        self.profileImage.image = self.uinfo.profile
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
