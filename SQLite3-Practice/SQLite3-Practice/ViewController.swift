e64b0d93dfc4360c38348652da03cea18390da64
eunaejang <dmsdo1234@gmail.com>
2021-10-15 15:14:57 +0900

    데이터베이스 파일 템플릿 사용하기

--- SQLite3-Practice/SQLite3-Practice/ViewController.swift
+++ SQLite3-Practice/SQLite3-Practice/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController {
         var stmt: OpaquePointer? = nil // 컴파일된 SQL을 담을 객체
         
         // 앱 내 문서 디렉터리 경로에서 SQLite DB 파일을 찾는다.
-        //let fileMgr = FileManager() // 파일 매니저 객체 생성
-        //let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first! // 생성된 매니저 객체를 사용하여 앱 내의 문서 디렉터리 경로를 찾고, 이를 URL 객체로 생성합니다.
-        //let dbPath = docPathURL.appendingPathComponent("db.sqlite").path // URL 객체에 "db.sqlite" 팡리 경로를 추가한 SQLite3 데이터베이스 경로를 만들어 냅니다.
-        let dbPath = "/Users/eunae/Developer/SQLite-workspace/db.sqlite" // 작성한 구문들이 데이터베이스에서 제대로 실행되었는지 여부를 확인하기 위한 임시 작업
+        let fileMgr = FileManager() // 파일 매니저 객체 생성
+        let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first! // 생성된 매니저 객체를 사용하여 앱 내의 문서 디렉터리 경로를 찾고, 이를 URL 객체로 생성합니다.
+        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path // URL 객체에 "db.sqlite" 팡리 경로를 추가한 SQLite3 데이터베이스 경로를 만들어 냅니다.
+        
+        // dbPath 경로에 파일이 없다면 앱 번들에 만들어 둔 db.sqlite를 가져와 복사한다.
+        if fileMgr.fileExists(atPath: dbPath) == false { // dbPath 경로에 파일이 있는지 없는지 체크
+            let dbSource = Bundle.main.path(forResource: "db", ofType: "sqlite") // 만약 파일이 없다면 앱 변들에 포함된 db.sqlite 파일의 경로를 읽어온다.
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath) // 번들 파일 경로에 있는 db.sqlite 파일을 dbPath 경로에 복사한다.
+        }
         
         let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
         
