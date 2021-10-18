d2f7a3c69f26b11fff9ecbc59489acd6bbc7e522
eunaejang <dmsdo1234@gmail.com>
2021-10-18 15:58:11 +0900

    스토리보드 및 기본 컨트롤러 클래스 준비

---
+++ HR/HR/DepartmentListVC.swift
@@ -0,0 +1,12 @@
+//
+//  DepartmentListVC.swift
+//  HR
+//
+//  Created by eunae on 2021/10/18.
+//
+
+import UIKit
+
+class DepartmentListVC : UITableViewController {
+    
+}
---
+++ HR/HR/EmployeeListVC.swift
@@ -0,0 +1,12 @@
+//
+//  EmployeeListVC.swift
+//  HR
+//
+//  Created by eunae on 2021/10/18.
+//
+
+import UIKit
+
+class EmployeeListVC : UITableViewController {
+    
+}
