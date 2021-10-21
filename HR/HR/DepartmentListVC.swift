343e580f747eac09bb9fdb8412a3e420251632b2
eunaejang <dmsdo1234@gmail.com>
2021-10-21 16:24:21 +0900

    부서 목록 구현

--- HR/HR/DepartmentListVC.swift
+++ HR/HR/DepartmentListVC.swift
@@ -16,8 +16,8 @@ class DepartmentListVC : UITableViewController {
         let dummyView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         self.tableView.tableFooterView = dummyView
         
-        // 화면이 로딩될 때 UI도 초기화
-        self.initUI()
+        self.departList = self.departDAO.find() // 기존 저장된 부서 정보를 가져온다.
+        self.initUI()// 화면이 로딩될 때 UI도 초기화
     }
     
     // UI 초기화 함수
@@ -35,5 +35,24 @@ class DepartmentListVC : UITableViewController {
         
         // 3. 셀을 스와이프했을 때 편집 모드가 되도록 설정
         self.tableView.allowsSelectionDuringEditing = true
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.departList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // indexPath 매개변수가 가리키는 행에 대한 데이터를 읽어온다.
+        let rowData = self.departList[indexPath.row]
+        
+        // 셀 객체를 생성하고 데이터를 배치한다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DEPART_CELL")
+        
+        cell?.textLabel?.text = rowData.departTitle
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell? .detailTextLabel?.text = rowData.departAddr
+        cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
+        
+        return cell!
     }
 }
