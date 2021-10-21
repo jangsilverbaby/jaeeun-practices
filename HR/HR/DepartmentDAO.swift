d861402ff8f2aaec2dec26150cb90d9bcc6791a4
eunaejang <dmsdo1234@gmail.com>
2021-10-21 15:06:59 +0900

    부서 목록 구현을 위한 스토리보드 설정 및 기본 코드 작성

--- HR/HR/DepartmentDAO.swift
+++ HR/HR/DepartmentDAO.swift
@@ -5,6 +5,8 @@
 //  Created by eunae on 2021/10/18.
 //
 
+import CoreGraphics
+
 class DepartmentDAO {
     // 부서 정보를 담을 튜플 타입 정의
     typealias DepartRecord = (Int, String, String)
--- HR/HR/DepartmentListVC.swift
+++ HR/HR/DepartmentListVC.swift
@@ -9,4 +9,31 @@ import UIKit
 
 class DepartmentListVC : UITableViewController {
     
+    var departList: [(departCd: Int, departTitle: String, departAddr: String)]! // 데이터 소스용 멤버 변수
+    let departDAO = DepartmentDAO() // SQLite 처리를 담당할 DAO 객체
+    
+    override func viewDidLoad() {
+        let dummyView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        self.tableView.tableFooterView = dummyView
+        
+        // 화면이 로딩될 때 UI도 초기화
+        self.initUI()
+    }
+    
+    // UI 초기화 함수
+    func initUI() {
+        // 1. 내비게이션 타이틀용 레이블 속성 설정
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "부서 목록 \n" + "총 \(self.departList.count) 개"
+        
+        // 2. 내비게이션 바 UI 설정
+        self.navigationItem.titleView = navTitle
+        self.navigationItem.leftBarButtonItem = self.editButtonItem // 편집 버튼 추가
+        
+        // 3. 셀을 스와이프했을 때 편집 모드가 되도록 설정
+        self.tableView.allowsSelectionDuringEditing = true
+    }
 }
