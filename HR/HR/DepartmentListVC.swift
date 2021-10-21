f2b2c28c11a465c118eae504bdbd7913582fc350
eunaejang <dmsdo1234@gmail.com>
2021-10-21 17:08:36 +0900

    등록 기능 구현

--- HR/HR/DepartmentListVC.swift
+++ HR/HR/DepartmentListVC.swift
@@ -55,4 +55,32 @@ class DepartmentListVC : UITableViewController {
         
         return cell!
     }
+    
+    // 신규 부서를 추가하는 메소드
+    @IBAction func add(_ sender: Any) {
+        // 여기에 비즈니스 로직이 들어갑니다.
+        let alert = UIAlertController(title: "신규 부서 등록", message: "신규 부서를 등록해 주세요", preferredStyle: .alert)
+        
+        // 부서명 및 주소 입력용 덱스트 필드 추가
+        alert.addTextField() { (tf) in tf.placeholder = "부서명" }
+        alert.addTextField() { (tf) in tf.placeholder = "주소" }
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel)) // 취소 버튼
+        alert.addAction(UIAlertAction(title: "확인", style: .default) { (_) in // 확인 버튼
+            // 부서 등록 로직이 들어갈 자리입니다.
+            let title = alert.textFields?[0].text // 부서명
+            let addr = alert.textFields?[1].text // 부서 주소
+            
+            if self.departDAO.create(title: title!, addr: addr!) {
+                // 신규 부서가 등록됨녀 DB에서 목록을 다시 읽어온 후, 테이블을 갱신해 준다.
+                self.departList = self.departDAO.find()
+                self.tableView.reloadData()
+                
+                // 내비게이션 타이틀에도 변경된 부서 정보를 반영한다.
+                let navTitle  = self.navigationItem.titleView as! UILabel
+                navTitle.text = "부서 목록 \n" + "총 \(self.departList.count) 개"
+            }
+        })
+        self.present(alert, animated: false)
+    }
 }
