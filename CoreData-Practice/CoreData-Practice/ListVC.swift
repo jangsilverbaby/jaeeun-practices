1bda155a3ee44d15e38a1d2bfd65a2235f320763
eunaejang <dmsdo1234@gmail.com>
2021-10-28 17:54:01 +0900

    등록 기능 구현하기

--- CoreData-Practice/CoreData-Practice/ListVC.swift
+++ CoreData-Practice/CoreData-Practice/ListVC.swift
@@ -27,6 +27,59 @@ class ListVC : UITableViewController {
         return result
     }
     
+    // 데이터를 저장할 메소드
+    func save(title: String, contents: String) -> Bool {
+        // 1. 앱 델리게이트 객체 참조
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // 2. 관리 객체 컨텍스트 참조
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // 3. 관리 객체 생성 & 값을 설정
+        let object = NSEntityDescription
+            .insertNewObject(forEntityName: "Board", into: context)
+        object.setValue(title, forKey: "title")
+        object.setValue(contents, forKey: "contents")
+        object.setValue(Date(), forKey: "regdate")
+        
+        // 4. 영구 저장소에 커밋되고 나면 list 프로퍼티에 추가한다.
+        do {
+            try context.save()
+            self.list.append(object) // 굳이 다시 데이터를 읽어오지 않아도 되도록 처리
+            return true
+        } catch {
+            context.rollback() // 마지막 동기화 시점 이후에 모든 변경 내역을 원래대로 되돌리는 역할, 영구 저장소에 커밋이 실패했다 하더라도 현재의 컨텍스트에는 새로 생성된 객체가 남아있으므로 그래로 두면 실제 저장소와 일시적으로 데이터가 일치하지 않는 문제가 생길 수 있으므로 컨텍스트를 롤백시켜주는 처리를 하는 것이 좋다.
+            return false
+        }
+    }
+    
+    // 데이터 저장 버튼에 대한 액션 메소드
+    @objc func add(_ sender: Any) {
+        let alert = UIAlertController(title: "게시글 등록", message: nil, preferredStyle: .alert)
+        
+        // 입력 필드 추가(이름 & 전화번호)
+        alert.addTextField() { $0.placeholder = "제목" }
+        alert.addTextField() { $0.placeholder = "내용" }
+        
+        // 버튼 추가(Cancel & Save)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default) {(_) in
+            guard let title = alert.textFields?.first?.text, let contents = alert.textFields?.last?.text else {
+                return
+            }
+            // 값을 저장하고 성공히면 테이블 뷰를 리로드한다.
+            if self.save(title: title, contents: contents) == true {
+                self.tableView.reloadData()
+            }
+        }) // end of alert.addAction(..
+        self.present(alert, animated: false)
+    }
+    
+    // 화면 및 로직 초기화 메소드
+    override func viewDidLoad() {
+        let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add(_:)))
+        self.navigationItem.rightBarButtonItem = addBtn
+    }
+    
     // 테이블 뷰 데이터 소스용 프로토콜 메소드
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.list.count
