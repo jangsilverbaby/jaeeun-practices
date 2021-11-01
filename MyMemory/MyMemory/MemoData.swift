80fec549fc87aff324c0f25ba6fc0cb4313fc5ce
eunaejang <dmsdo1234@gmail.com>
2021-11-01 16:03:41 +0900

    코어 데이터로 로직 구현을 위한 MemoData 수정

--- MyMemory/MyMemory/MemoData.swift
+++ MyMemory/MyMemory/MemoData.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class MemoData {
     var memoIdx : Int? // 데이터 식별값
@@ -13,4 +14,5 @@ class MemoData {
     var contents : String? // 메모 내용
     var image : UIImage? // 이미지
     var regdate : Date? // 작성일
+    var objectID: NSManagedObjectID? // 원본 MomoMO 객체를 참조하기 위한 속성
 }
