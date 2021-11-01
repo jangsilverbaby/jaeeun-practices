bacb2c223d84398df9447814358e75dd739cfc79
eunaejang <dmsdo1234@gmail.com>
2021-11-01 12:55:35 +0900

    Initial Commit

---
+++ ManualCoreData/ManualCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualCoreData
+//
+//  Created by eunae on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
