e28af90718b41d4ea4703958c69ca122b59080ea
eunaejang <dmsdo1234@gmail.com>
2021-11-03 15:56:51 +0900

    GET 방식으로 현재 시간 확인용 API 호출하기

--- APITest/APITest/ViewController.swift
+++ APITest/APITest/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func callCurrentTime(_ sender: Any) {
+        do {
+            // 1. URL 설정 및 GET 방식으로 API 호출
+            let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/currentTime")
+            let response = try String(contentsOf: url!)
+            
+            // 2. 읽어온 값을 레이블에 표시
+            self.currentTime.text = response
+            self.currentTime.sizeToFit()
+        } catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
+    
 }
 
