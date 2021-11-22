be5e33ff9dfdea35cae5b4da58ea823562ab1e8b
eunaejang <dmsdo1234@gmail.com>
2021-11-22 12:12:43 +0900

    계정 등록 기능 구현을 위한 스토리보드 구현 및 기본 작업

---
+++ MyMemory/MyMemory/JoinVC.swift
@@ -0,0 +1,18 @@
+//
+//  JoinVC.swift
+//  MyMemory
+//
+//  Created by eunae on 2021/11/22.
+//
+
+import UIKit
+import Alamofire
+
+class JoinVC: UIViewController {
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func submit(_sender: Any) {
+        
+    }
+}
