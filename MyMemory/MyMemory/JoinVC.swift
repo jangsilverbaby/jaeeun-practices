accde52e7e400b0857500e9ee927c749c4ff662c
eunaejang <dmsdo1234@gmail.com>
2021-11-22 14:27:37 +0900

    프로필 이미지 설정 기능 구현하기

--- MyMemory/MyMemory/JoinVC.swift
+++ MyMemory/MyMemory/JoinVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Alamofire
 
-class JoinVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class JoinVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet weak var profile: UIImageView!
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,6 +21,53 @@ class JoinVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // 테이블 뷰의 dataSource, delegate 속성 지정
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        // 프로필 이미지를 원형으로 설정
+        self.profile.layer.cornerRadius = self.profile.frame.width / 2
+        self.profile.layer.masksToBounds = true
+        
+        // 프로필 이미지에 탭 제스처 및 액션 이벤트 설정
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tappedProfile(_:)))
+        self.profile.addGestureRecognizer(gesture)
+    }
+    
+    @objc func tappedProfile(_ sender: Any) {
+        // 전반부) 원하는 소스 타입을 선택할 수 있는 액션 시트 구현
+        let msg = "프로필 이미지를 읽어올 곳을 선택하세요."
+        let sheet = UIAlertController(title: msg, message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "취소", style: .cancel))
+        sheet.addAction(UIAlertAction(title: "저장된 앨범", style: .default) { (_) in
+            selectLibrary(src: .savedPhotosAlbum) // 저장된 앨범에서 이미지 선택하기
+        })
+        sheet.addAction(UIAlertAction(title: "포토 라이브러리", style: .default) { (_) in
+            selectLibrary(src: .photoLibrary) // 포토 라이브러리에서 이미지 선택하기
+        })
+        sheet.addAction(UIAlertAction(title: "카메라", style: .default) { (_) in
+            selectLibrary(src: .camera) // 카메라에서 이미지 촬영하기
+        })
+        self.present(sheet, animated: false)
+        
+        // 후반부) 전달된 소스 타입에 맞게 이미지 피커 창을 여낸 내부 함수
+        func selectLibrary(src: UIImagePickerController.SourceType) {
+            if UIImagePickerController.isSourceTypeAvailable(src) {
+                let picker = UIImagePickerController()
+                picker.delegate = self
+                picker.allowsEditing = true
+                
+                self.present(picker, animated: false)
+            } else {
+                self.alert("사용할 수 없는 타입입니다.")
+            }
+        }
+    }
+    
+    // 이미지 선택 시 실행되는 델리게이트 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let rawVal = UIImagePickerController.InfoKey.originalImage.rawValue
+        if let img = info[UIImagePickerController.InfoKey(rawValue: rawVal)] as? UIImage {
+            self.profile.image = img
+        }
+        self.dismiss(animated: true)
     }
     
     @IBAction func submit(_sender: Any) {
